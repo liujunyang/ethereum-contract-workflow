d28db6ee3c0de061ce86f3be67ff5926b36803a2
liujunyang <liujunyangscript@gmail.com>
2018-06-02 20:42:54 +0800

    save to disk

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,9 +1,23 @@
-const fs = require('fs')
+const fs = require('fs-extra')
 const path = require('path')
 const solc = require('solc')
 
+// cleanup
+const compiledDir = path.resolve(__dirname, '../compiled')
+fs.removeSync(compiledDir)
+fs.ensureDirSync(compiledDir)
+
+// compile
 const contractPath = path.resolve(__dirname, '../contracts', 'Car.sol')
 const contractSource = fs.readFileSync(contractPath, 'utf8')
 const result = solc.compile(contractSource, 1)
 
-console.log(99, result);
+// save to disk
+Object.keys(result.contracts).forEach(name => {
+  const contractName = name.replace(/^:/, '')
+  const filePath = path.resolve(compiledDir, `${contractName}.json`)
+  
+  fs.outputJsonSync(filePath, result.contracts[name])
+  console.log(`saved compiled contract ${contractName} to ${filePath}`)
+})
+
