cde38ff18ca90f8b4449540dc9be0d6bea424989
liujunyang <liujunyangscript@gmail.com>
2018-06-10 12:03:45 +0800

    add test

--- tests/car.spec.js
+++ tests/car.spec.js
@@ -8,3 +8,39 @@ const contractPath = path.resolve(__dirname, '../compiled/Car.json')
 const {interface, bytecode} = require(contractPath)
 
 // 配置 provider
+const web3 = new Web3(ganache.provider())
+
+let accounts
+let contract
+const initialBrand = 'AUDI'
+
+describe('contract', () => {
+  // 每次跑单测都要部署全新的合约实例，起到隔离的作用
+  beforeEach(async () => {
+    accounts = await web3.eth.getAccounts()
+    console.log('合约部署账户', accounts[0]);
+
+    contract = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({data: bytecode, arguments: [initialBrand]})
+      .send({from: accounts[0], gas: '1000000'})
+
+    console.log('合约部署成功', contract.options.address);
+    
+  })
+
+  it('deploy a contract', () => {
+    assert.ok(contract.options.address)
+  })
+
+  it('has initial brand', async () => {
+    const brand = await contract.methods.brand().call()
+    assert.equal(brand, initialBrand)
+  })
+
+  it('can change the brand', async () => {
+    const newBrand = 'BMW'
+    await contract.methods.setBrand(newBrand).send({from: accounts[0]})
+    const brand = await contract.methods.brand().call()
+    assert.equal(brand, newBrand)
+  })
+})
