65e55dc24955c37e3ca5975c622aba3551144ed3
liujunyang <liujunyangscript@gmail.com>
2018-06-24 11:38:11 +0800

    add project test

--- scripts/compile.js
+++ scripts/compile.js
@@ -2,27 +2,32 @@ const fs = require('fs-extra')
 const path = require('path')
 const solc = require('solc')
 
-// cleanup
+// 1.cleanup
 const compiledDir = path.resolve(__dirname, '../compiled')
 fs.removeSync(compiledDir)
 fs.ensureDirSync(compiledDir)
 
-// compile
-const contractPath = path.resolve(__dirname, '../contracts', 'Car.sol')
-const contractSource = fs.readFileSync(contractPath, 'utf8')
-const result = solc.compile(contractSource, 1)
+// 2.search all contracts
+const contractFiles = fs.readdirSync(path.resolve(__dirname, '../contracts'))
 
-// check errors
-if (Array.isArray(result.errors) && result.errors.length) {
-  throw new Error(result.errors[0])
-}
+contractFiles.forEach(contractFile => {
+  // compile
+  const contractPath = path.resolve(__dirname, '../contracts', contractFile)
+  const contractSource = fs.readFileSync(contractPath, 'utf8')
+  const result = solc.compile(contractSource, 1)
+  console.log(`file compiled: ${contractFile}`)
 
-// save to disk
-Object.keys(result.contracts).forEach(name => {
-  const contractName = name.replace(/^:/, '')
-  const filePath = path.resolve(compiledDir, `${contractName}.json`)
-  
-  fs.outputJsonSync(filePath, result.contracts[name])
-  console.log(`saved compiled contract ${contractName} to ${filePath}`)
+  // check errors
+  if (Array.isArray(result.errors) && result.errors.length) {
+    throw new Error(result.errors[0])
+  }
+
+  // save to disk
+  Object.keys(result.contracts).forEach(name => {
+    const contractName = name.replace(/^:/, '')
+    const filePath = path.resolve(compiledDir, `${contractName}.json`)
+    
+    fs.outputJsonSync(filePath, result.contracts[name])
+    console.log(`saved compiled contract ${contractName} to ${filePath}`)
+  })
 })
-
