99dba162d18b1e53eed60463748a107657e68333
liujunyang <liujunyangscript@gmail.com>
2018-06-24 16:43:07 +0800

    add ui

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import {Button} from '@material-ui/core'
+import withRoot  from '../libs/withRoot'
+import Layout from '../components/Layout'
 
-export default class Index extends React.Component {
+class Index extends React.Component {
   render() {
-    return <div>Welcome to Ethereum ICO DApp!</div>
+    return (
+      <Layout>
+        <Button variant="raised" color="primary">
+          Welcome to Ethereum ICO DApp!
+        </Button>
+      </Layout>
+    );
   }
 }
+
+export default withRoot(Index)
