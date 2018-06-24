9bc61a1a38a46811cb51329d7e14fb31c79b0547
liujunyang <liujunyangscript@gmail.com>
2018-06-24 17:17:28 +0800

    list my money

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import {Button} from '@material-ui/core'
+
+import web3 from '../libs/web3'
 import withRoot  from '../libs/withRoot'
 import Layout from '../components/Layout'
 
 class Index extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      accounts: [],
+    }
+  }
+
+  async componentDidMount() {
+    
+    const accounts = await web3.eth.getAccounts()
+    const balances = await Promise.all(accounts.map(x => web3.eth.getBalance(x)))
+    console.log({accounts, balances})
+
+    this.setState({accounts: accounts.map((x, i) => ({account: x, balance: balances[i]}))})
+  }
+
   render() {
+    const {accounts} = this.state
     return (
       <Layout>
-        <Button variant="raised" color="primary">
-          Welcome to Ethereum ICO DApp!
-        </Button>
+        <ul>
+        {accounts.map(x => (
+          <li key={x.account}>
+            {x.account} => {web3.utils.fromWei(x.balance, 'ether')} ETH
+          </li>
+        ))}
+        </ul>
       </Layout>
     );
   }
